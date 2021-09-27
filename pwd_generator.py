96cf0726d14287ae122150bbf60a632c0f01d981
Kushal-S-BAstakoti <sharmakushalbastakoti@gmail.com>
2021-09-27 02:12:10 -0700

    Final Update

--- pwd_generator.py
+++ pwd_generator.py
@@ -3,10 +3,20 @@
 
 #You can customize the weight,ratio and number of digits by just editing some values below 
 
+#To use custom value run as python pwd_generator.py number1
+#number1 is the number of digits you want
 
+#to customize ratio of strings letters and digits
+#run python pwd_generator.py num1 num2 num3 num4
+#num1 is digits 
+#num2 is weight of strings
+#num3 is weight of numbers
+#num3 is weight of symbols
 
 import numpy as np
-import random as rm 
+import random as rm
+import sys
+
 
 #List for base_digits
 word_list   =   np.array(['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F','g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L','m', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R','s', 'S', 't', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', ])
@@ -15,15 +25,30 @@ number_list =   np.array(['1', '2', '3' , '4', '5', '6', '7','8', '9'])
 
 symbol_list =   np.array(['!','@','#','$','%','^','&','*'])
 
-#for 16 digits take 60% digits string, take 25% numbers and 15 % symbols;
-word_count      =   16
-weight_string   =   .60
-weight_number   =   .25
-weight_symbols  =   .15
 
-ratio_string    =   (int) (word_count * weight_string)
-ratio_numbers   =   (int) (word_count * weight_number)
-ratio_symbols   =   (int) (word_count * weight_symbols)
+#for 16 digits take 60% digits string, take 25% numbers and 15 % symbols; 
+#Defalut Values
+word_count      =   16
+weight_string   =   60
+weight_number   =   25
+weight_symbols  =   15
+
+if len(sys.argv) == 2 :
+    word_count      =   int(sys.argv[1])
+elif len(sys.argv) == 5:
+    word_count      =   int(sys.argv[1])
+    weight_string   =   int(sys.argv[2])
+    weight_number   =   int(sys.argv[3])
+    weight_symbols  =   int(sys.argv[4])
+else:
+    print("Invalid number of Arguments")
+    exit(0)
+
+
+ratio_total     =   weight_number+weight_string+weight_symbols
+ratio_string    =   (int) (word_count * weight_string)/ ratio_total
+ratio_numbers   =   (int) (word_count * weight_number)/ratio_total
+ratio_symbols   =   (int) (word_count * weight_symbols)/ratio_total
 
 
 #Code for filling in any gaps that occured when creating integer ratios
@@ -49,16 +74,19 @@ while (raw_password.size - ratio_string < ratio_numbers):
 while (raw_password.size -ratio_numbers -ratio_string < ratio_symbols):
     raw_password    =   np.append(raw_password, rm.choice(symbol_list))
 
-dict1   ={k:True for k in raw_password}
+
+#Dictionary to avoid repitition
+dict1   ={k:np.count_nonzero(raw_password == k) for k in raw_password}
 
 password    =   np.array([])
 
 #Final Password
 while(password.size < word_count):
     a = rm.choice(raw_password)
-    if(dict1[a] == True):
+    if(dict1[a] != 0 ):
         password = np.append(password,a)
-        dict1[a] = False
+        dict1[a] -= 1
 
 
+print("".join(password))
 
